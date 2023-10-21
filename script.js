a8a87f83c985b049842bfcd7c70c7b3568189f3c
mariabernades23 <maria.lopes10@estudante.ifms.edu.br>
2023-10-21 15:19:19 -0400

    Configurando Access-Control-Allow-Origin

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Origin', 'https://gps-app-git-main-mariabernardes23s-projects.vercel.app/'); 
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
